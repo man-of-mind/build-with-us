65ef5e5fce89a336d02d19989c055c5689a84030
Abiodun Ganiyu <timothyabiodun300@gmail.com>
2022-09-22 23:17:33 +0100

    made changes according to new design

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Services.module.scss";
 import iQubeLogo from '../Images/iqube.svg';
+import ellipse from '../Images/ellipse.svg';
 
 const Services = () => {
     return (
@@ -8,9 +9,14 @@ const Services = () => {
             <figure>
                 <img src={iQubeLogo} alt="iQube logo" />
             </figure>
-            <span>SOFTWARE DEVELOPMENT</span>
-            <span>DATA SCIENCE CONSULTANCY</span>
-            <span>IVAS&#40;INTELLIGENT VALUE ADDED SERVICES&#41;</span>
+            <div className={styles['categories']}>
+                <span>SOFTWARE DEVELOPMENT</span>
+                <img src={ellipse} alt="dot sign"/>
+                <span>DATA SCIENCE CONSULTANCY</span>
+                <img src={ellipse} alt="dot sign"/>
+                <span>IVAS &#40;INTELLIGENT VALUE ADDED SERVICES&#41;</span>
+            </div>
+            
         </div>
     );
 }
